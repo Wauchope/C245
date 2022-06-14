59e0e79cc6c3842c36b6eaedfd2266e53db08b45
= <jordan.wauchope@gmail.com>
2022-06-14 15:05:20 +0100

    Exercise 2 from worksheet
    Three methods to test whether a character is a vowel
    
    Method 1:
    
    IsCharVowel takes a character, converts it to lowercase and returns true if it's a vowel. Otherwise, returns false
    
    Method 2:
    
    IsFirstCharacterOfStringVowel takes a string and checks if the first character of that string is a vowel using method 1
    
    Method 3
    IsCharacterAtIndexOfStringVowel takes a string and an integer and checks if
      a) the index is valid (i.e. within the bounds of the string.
        If it's not, return false
        otherwise,
      b) the character at index is a vowel using method 1

---
+++ src/Day1/VowelChecker.java
@@ -0,0 +1,51 @@
+package Day1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VowelChecker {
+    final static char[] VOWELS = {'a', 'e', 'i', 'o', 'u'};
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a string with a length greater than 3");
+        String text = in.next();
+
+        //Simple test, is a character a vowel
+        char simpleTestChar = 'b';
+        char simpleTestChar2 = 'a';
+        System.out.println(IsCharVowel(simpleTestChar)); // Should return false
+        System.out.println(IsCharVowel(simpleTestChar2)); // Should return true
+
+        System.out.println(IsFirstCharacterOfStringVowel(text)); // With input text banana this should return false
+        System.out.println(IsCharacterAtIndexOfStringVowel(text, 3)); //With input text banana this should return true
+
+    }
+
+    private static boolean IsCharVowel(char character)
+    {
+        for (char vowel : VOWELS)
+        {
+            if (vowel == character) return true;
+        }
+
+        return false;
+    }
+
+    private static boolean IsFirstCharacterOfStringVowel(String textToCheck)
+    {
+        char firstChar = textToCheck.toLowerCase(Locale.ROOT).charAt(0);
+        return IsCharVowel(firstChar);
+    }
+
+    private static boolean IsCharacterAtIndexOfStringVowel(String textToCheck, int index)
+    {
+        if (index < 0 || index >= textToCheck.length()) {
+            System.out.println("There is no character at this index. Returning false.");
+            return false;
+        }
+        char characterToCheck = textToCheck.toLowerCase(Locale.ROOT).charAt(index);
+        return IsCharVowel(characterToCheck);
+    }
+}
