40981051e4cf714a99d1a754595d19ad407c50d8
= <jordan.wauchope@gmail.com>
2022-06-15 17:00:47 +0100

    Update Helper.java
    Added two methods, one to pick a random value from an enumerator and one which supports this (by creating a Random object if it doesn't exist. This random object is public for now, just incase I need to use it later.
    
    Consider moving these static variables and their creators into a new class perhaps using the singleton pattern.

--- src/Other/Helper.java
+++ src/Other/Helper.java
@@ -1,9 +1,12 @@
 package Other;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Helper {
     static Scanner in;
+    //Could probably introduce some kind of noise function but for the purpose of this course I don't think I need to
+    public static Random random;
 
     /**
      * Reads a value from default System.in and returns it so long as the value entered is an integer
@@ -118,6 +121,27 @@ public class Helper {
         System.out.println("Press enter to continue...");
         in.nextLine();
         in.nextLine();
+    }
+
+
+    /**
+     * Initializes the scanner object "in" with a new Scanner if it does not already exist
+     */
+    private static void CreateRandomIfNull() {
+        if (random != null) return;
+
+        random = new Random();
+    }
+
+    /**
+     * Allows you to pick a random value from an enumerator
+     * @param myClass The enum you wish to pick randomly from
+     * @return A random value from an enumerator
+     */
+    public static <T extends Enum<?>> T GetRandomFromEnum(Class<T> myClass)
+    {
+        int x = random.nextInt(myClass.getEnumConstants().length);
+        return myClass.getEnumConstants()[x];
     }
 
 }
