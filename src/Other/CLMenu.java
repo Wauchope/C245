6e1b0cb87e23cfc1833dd62329d88d51e5351352
= <jordan.wauchope@gmail.com>
2022-06-15 16:56:41 +0100

    Create CLMenu.java
    Created a class for using a CL Menu. Hopefully it works.
    
    Takes two inputs, an array of strings (the options you pick from), and an array of Runnables. I know this works with lamba functions, so i'll stick with using those for now.

---
+++ src/Other/CLMenu.java
@@ -0,0 +1,46 @@
+package Other;
+
+import java.util.function.Function;
+
+import static Other.Helper.ReadInt;
+
+public class CLMenu {
+    String[] options;
+    Runnable[] methods;
+    public CLMenu(String[] options, Runnable[] methods)
+    {
+        if (options.length != methods.length)
+        {
+            throw new IllegalArgumentException("Options and methods must be the same length.");
+        }
+
+        this.options = options;
+        this.methods = methods;
+    }
+
+    public void StartMenu()
+    {
+        int choice;
+        do {
+            //Display options
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ": " + options[i]);
+            }
+            System.out.println((options.length + 1) + ": Exit program");
+
+            //Get user input and ensure its in the correct range
+            do {
+                choice = ReadInt("Please select an option: ", "Please enter a number between 1 and " + options.length + ":");
+            } while (choice < 1 || choice > options.length + 1);
+
+            //As long as we're not trying to exit
+            if (choice != options.length + 1) {
+                //Run the relevant method
+                methods[choice].run();
+            }
+        }
+        while(choice != options.length+1);
+
+        System.out.println("Thank you for running my program! Goodbye.");
+    }
+}
